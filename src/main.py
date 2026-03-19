3353ffc2f02ddb17a06bc761afc4cecd195bee6b
543872524 <543872524@qq.com>
2026-03-19 21:31:37 +0800

    添加一个文件系统的register

--- src/main.py
+++ src/main.py
@@ -1,5 +1,14 @@
+from mcp.server.fastmcp import FastMCP
+
+from mcp_register import register_filesystem_mcp
+
+mcp = FastMCP(name='mcp-server-project', port=8880)
+
+register_filesystem_mcp(mcp)
+
+
 def main():
-    print("Hello from mcp-server-project!")
+    mcp.run(transport='sse')
 
 
 if __name__ == "__main__":
